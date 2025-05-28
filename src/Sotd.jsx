a38261d2a9c808c3419e31fdea461245352fa687
kanishka-commits <kanishka03p@gmail.com>
2025-05-28 15:12:35 +0530

    Updated Sotd

---
+++ src/Sotd.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sotd.css"
+
+const Sotd = () => {
+  return (
+    <>
+    <div className="vertical-line">
+    “(Your Sphere of the Day will be shown here)”
+    </div>
+
+    </>
+  )
+}
+
+export default Sotd
